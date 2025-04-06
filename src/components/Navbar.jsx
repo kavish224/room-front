10c061dea556f8345a9ee7258922bdfe0722be4e
Kavish Ambani <kavishkkambani@gmail.com>
2025-04-06 05:39:55 +0530

    shadow change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-[#2D3A45] shadow-sm py-4 sticky top-0 z-50">
+    <nav className="bg-[#2D3A45] py-4 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto flex justify-between items-center px-6">
         <Link to="/" className="text-2xl font-bold text-white">
           RoomRental
